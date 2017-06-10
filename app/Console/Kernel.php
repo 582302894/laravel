442c78a1787c64a7daa27e1d816372ef1a210133
582302894 <582302894@qq.com>
2017-06-10 14:08:42 +0800

    kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use App\Http\Controllers\Spider\SpiderController;
 class Kernel extends ConsoleKernel
 {
     /**
@@ -26,6 +26,10 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->call(function(){
+            $spider=new SpiderController();
+            $spider->picture();
+        })->everyTenMinutes();
     }
 
     /**
