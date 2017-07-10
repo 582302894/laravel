b8bfbbeb618811f422b8d0dafefcdc18dfbe7cd3
DESKTOP-IJ18V9P\king <582302894@qq.com>
2017-07-10 10:55:28 +0800

    1

--- public/self/tree/test.php
+++ public/self/tree/test.php
@@ -7,14 +7,22 @@ class tree
     public $right = null;
 }
 
-$a = new tree(['a']);
-$b = new tree(['b']);
-$c = new tree(['c']);
-$d = new tree(['d']);
-$e = new tree(['e']);
-$f = new tree(['f']);
-$g = new tree(['g']);
-$h = new tree(['h']);
+$a        = new tree();
+$a->value = 'a';
+$b        = new tree();
+$b->value = 'b';
+$c        = new tree();
+$c->value = 'c';
+$d        = new tree();
+$d->value = 'd';
+$e        = new tree();
+$e->value = 'e';
+$f        = new tree();
+$f->value = 'f';
+$g        = new tree();
+$g->value = 'g';
+$h        = new tree();
+$h->value = 'h';
 
 $a->left  = $b;
 $a->right = $c;
@@ -30,9 +38,50 @@ $f->right = $h;
 
 function draw($root)
 {
- 
+    if ($root->left != null) {
+        echo $root->value . "->left:" . $root->left->value . "&nbsp;";
+    }
+    if ($root->right != null) {
+        echo $root->value . "->right:" . $root->right->value;
+    }
+    if ($root->right != null || $root->left != null) {
+        echo "<br/>";
+    }
+    if ($root->left != null) {
+        draw($root->left);
+    }
+    if ($root->right != null) {
+        draw($root->right);
+    }
 }
 draw($a);
+
+$deep     = 0;
+$deeepNum = [];
+function deep($root)
+{
+    global $deep;
+    global $deeepNum;
+    $deep++;
+    if (!isset($deeepNum[$deep])) {
+        $deeepNum[$deep] = 1;
+    } else {
+        $deeepNum[$deep]++;
+    }
+    if (!empty($root->left)) {
+        deep($root->left);
+    }
+    if (!empty($root->right)) {
+
+        deep($root->right);
+    }
+    $deep--;
+
+}
+deep($a);
+echo '<pre>';
+var_dump($deeepNum);
+echo '</pre>';
 ?>
 
 <!DOCTYPE html>
