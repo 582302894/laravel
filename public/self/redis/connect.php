47f1014d06a5113b09cd203397a63f2290befc46
582302894 <582302894@qq.com>
2017-07-31 22:04:14 +0800

    redis

--- public/self/redis/connect.php
+++ public/self/redis/connect.php
@@ -1,16 +1,27 @@
 <?php
-
+// ini_set('default_socket_timeout', 20);
+// echo '<pre>';
+// var_dump(ini_get('default_socket_timeout'));
+// echo '</pre>';exit();
 try {
-	if(!class_exists('Redis')){
-		echo 'not exist class Redis';
-		exit();
-	}
+    if (!class_exists('Redis')) {
+        echo 'not exist class Redis';
+        exit();
+    }
     $redis = new Redis();
-    $flag=$redis->connect('127.0.0.1', 6379);
-    echo '<pre>';
-    var_dump($flag);
-    var_dump($redis);
-    echo '</pre>';
+    $redis->connect('127.0.0.1', 6379);
+
+
+    $value = $redis->get('test-redis');
+    if (empty($value)) {
+        $value = $_SERVER['REMOTE_ADDR'] . 'test-redis<br>';
+    } else {
+        $value .= $_SERVER['REMOTE_ADDR'] . 'test-redis<br>';
+    }
+    $redis->set('test-redis', $value);
+
+    echo $redis->get('test-redis');
+
 } catch (Exception $e) {
     echo '<pre>';
     var_dump($e);
